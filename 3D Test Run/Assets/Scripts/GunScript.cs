7394adf0e59655f4de6448853a32f18293cd6459
VaderPorpoise <vaderporpoise@gmail.com>
2020-12-27 01:09:02 +0200

    gun update

---
+++ 3D Test Run/Assets/Scripts/GunScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunScript : MonoBehaviour
+{
+    public GameObject startingGun;
+    public Transform GunStartingPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+       GameObject gun =  Instantiate(startingGun, GunStartingPoint);
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
