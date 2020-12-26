cab2739875381827cbf5c49a88a571aa6572539b
VaderPorpoise <vaderporpoise@gmail.com>
2020-12-26 20:18:48 +0200

    tested out the whole movement thingy

---
+++ 3D Test Run/Assets/Scripts/mouseLook.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseLook : MonoBehaviour
+{
+
+    [Header("variables")]
+
+    public float mouseSensitivity = 100f;
+
+    float xRotation = 0f;
+
+    public Transform playerBodyTrans;
+
+    [Header("cached components")]
+
+    public Transform playerTrans;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * mouseSensitivity;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerTrans.Rotate(Vector3.up * mouseX);
+        playerBodyTrans.Rotate(Vector3.up * mouseX);
+
+    }
+}
---
+++ 3D Test Run/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 12f;
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");// Input Axis Verticle is not setup. this error is given if you mispell it 
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+    }
+}
