f907e40a4467d498aa6c0b38fa22ee2a4875d6b6
VaderPorpoise <vaderporpoise@gmail.com>
2020-12-28 18:00:34 +0200

    making a pool

---
+++ 3D Test Run/Assets/Scripts/BulletPooling.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletPooling : MonoBehaviour
+{
+    [System.Serializable]
+    public class Pool
+    {
+        public string tag;
+        public GameObject prefab;
+        public int poolSize;
+    }
+    public List<Pool> pools;
+    public Dictionary<string, Queue<GameObject>> poolDictionary;
+
+    void Start()
+    {
+        poolDictionary = new Dictionary<string, Queue<GameObject>>();
+        foreach (Pool pool in pools)
+        {
+            Queue<GameObject> objectPool = new Queue<GameObject>();
+            for (int i = 0; i < pool.poolSize; i++)
+            {
+                GameObject bullet = Instantiate(pool.prefab);
+                bullet.SetActive(false);
+                objectPool.Enqueue(bullet);
+            }
+            poolDictionary.Add(pool.tag, objectPool);
+        }
+    }
+
+    public void SpawnFromPool(string tag, Vector3 position, Quaternion rotation)
+    {
+        GameObject objectToSpawn = poolDictionary[tag].Dequeue();
+        objectToSpawn.transform.position = position;
+        objectToSpawn.transform.rotation = rotation;
+        objectToSpawn.SetActive(true);
+
+    }
+    void Update()
+    {
+
+    }
+}
